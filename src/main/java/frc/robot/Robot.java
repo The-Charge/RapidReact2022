2fa5702aad2179cd80cc4d14dcd8bf2d12cc9e7d
TJ Neuenfeldt <neuenfeldttj@midlandps.org>
2022-01-25 20:23:04 -0500

    Coded inital synchronization. Robot moves forward, but left is ignored when pressure is put on it. Also, the inversions are different than the example code...

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -16,6 +16,7 @@ import edu.wpi.first.hal.FRCNetComm.tInstances;
 import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
