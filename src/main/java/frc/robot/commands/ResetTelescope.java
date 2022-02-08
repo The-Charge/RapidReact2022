c6f79e5615cdd59704ab856a7dbb831ec5a5cd87
TylerDBryant <tyler.bryant5@aol.com>
2022-02-07 20:29:46 -0500

    added one commet

--- src/main/java/frc/robot/commands/ResetTelescope.java
+++ src/main/java/frc/robot/commands/ResetTelescope.java
@@ -54,7 +54,7 @@ public class ResetTelescope extends CommandBase {
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {
+    public void execute() {//code to set motors here
     }
 
     // Called once the command ends or is interrupted.
